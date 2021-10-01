5ea22a05a750de6c173251bb04d25d0deff8520c
witzg <witzg@DESKTOP-T0UNDEI.lan>
2021-09-30 21:11:21 -0500

    fresh start refactoring for new javafx environment

---
+++ src/lexoperm/PermutationsLex_thread_queue4.java
@@ -0,0 +1,140 @@
+package lexoperm;
+
+
+import java.util.Random;
+import java.awt.*;
+import javax.swing.*;
+import org.jfugue.*;
+
+
+
+public class PermutationsLex_thread_queue4 {
+
+    public static void show(int[] a) {
+        JFrame frame = new JFrame ();
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        JPanel subPanel = new JPanel();
+        subPanel.setBackground (Color.pink);
+        subPanel.setPreferredSize (new Dimension(250,200));
+        for (int i = 0; i < a.length; i++) {
+            switch (a[i])
+            {
+                case (0): subPanel.setBackground(Color.LIGHT_GRAY);
+                case (1): subPanel.setBackground(Color.white);
+                case (2): subPanel.setBackground(Color.pink);
+                case (3): subPanel.setBackground(Color.red);
+                case (4): subPanel.setBackground(Color.pink);
+                case (5): subPanel.setBackground(Color.yellow);
+                case (6): subPanel.setBackground(Color.pink);
+                case (7): subPanel.setBackground(Color.blue);
+                case (8): subPanel.setBackground(Color.pink);
+                case (9): subPanel.setBackground(Color.cyan);
+                case (10): subPanel.setBackground(Color.pink);
+                case (11): subPanel.setBackground(Color.gray);
+            }   
+        frame.getContentPane().add(subPanel);
+        frame.pack();
+        frame.setVisible(true);
+          // try {
+          //     frame.wait(50);
+         //  } catch (InterruptedException ex) {
+          //    Logger.getLogger(PermutationsLex_thread_queue2.class.getName()).log(Level.SEVERE, null, ex);
+        //    }
+        //for (int j = 0; i < 6000; i++) { 
+        //    JLabel label1 = new JLabel ("Question authority,");
+      //   subPanel.add (label1);
+       // }
+
+        }
+        frame.dispose(); 
+    } 
+
+    public static void swap(int[] a, int i, int j) {
+        int temp = a[i];
+        a[i] = a[j];
+        a[j] = temp;
+    }
+    public static boolean hasNext(int[] a) {
+        int N = a.length;
+        // find rightmost element a[k] that is smaller than element to its right
+        int k; 
+        for (k = N-2; k >= 0; k--)
+            if (a[k] < a[k+1]) break;
+        if (k == -1) return false;
+        // find rightmost element a[j] that is larger than a[k]
+        int j = N-1;
+        while (a[k] > a[j])
+            j--;
+        swap(a, j, k);
+        for (int r = N-1, s = k+1; r > s; r--, s++)
+            swap(a, r, s);
+        return true;
+    }
+
+    static class perm implements Runnable {
+
+        public perm(){
+
+        }
+
+        private Thread tperm;
+
+        public void start (){
+           System.out.println("Starting permutations " );
+           if (tperm == null) {
+              tperm = new Thread (this);
+              tperm.start ();
+           }
+        }
+
+        public void run() {
+            // initialize permutation
+            int side_limit = 12;
+            int[] a  = new int[side_limit];
+            byte Rbyte;
+            int indec;
+            int offset;
+            Random generator = new Random();
+            Player player = new Player();
+            for (int i = 0; i < side_limit; i++)
+                a[i] = i;
+            while (hasNext(a)) {
+                for (int i = 0; i < side_limit; i++) {
+                    System.out.print(a[i] + ", ");
+                }
+                System.out.println();
+                PolygonDrawingFast myp = new PolygonDrawingFast(a);
+                myp.setVisible(true);
+                try{Thread.sleep(500);}catch(InterruptedException e){System.out.println(e);}
+                myp.dispose();
+            } //end while
+        }//end method Run
+    }//end class Perm
+
+    static class cubes implements Runnable {
+
+        public cubes(){
+
+        }
+        public void start ()
+        {
+         //  System.out.println("Starting permutations " );
+           if (tperm2 == null)
+           {
+              tperm2 = new Thread (this);
+              tperm2.start ();
+           }
+        }
+        private Thread tperm2;
+        public void run() {
+            System.out.println("beautiful feet beautiful soup");
+            //new PolygonDrawing().setVisible(true);
+        }
+
+    }//end cubes
+
+}//end PermutationsLex_thread_queue2.java
+
+
+
+
---
+++ src/lexoperm/PolygonDrawingFast.java
@@ -0,0 +1,111 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package lexoperm;
+
+/**
+ *
+ * @author Owner
+ */
+
+import java.awt.Graphics;
+import java.awt.Graphics2D;
+import java.awt.geom.Line2D;
+
+import javax.swing.JFrame;
+import javax.swing.SwingUtilities;
+
+
+/**
+ * This program demonstrates how to draw lines using Graphics2D object.
+ * @author www.codejava.net
+ *
+ */
+public class PolygonDrawingFast extends JFrame {
+    
+        static int sides = 12;
+        static int radius = 160;
+        static int startingX = 200;
+        static int startingY = 200;
+        static Double [] xarray = new Double [sides];
+        static Double [] yarray = new Double [sides];
+        static Integer [] permvector = new Integer [sides];
+
+	public PolygonDrawingFast(int [] a) {
+            super("Polygon Drawing");
+            setSize(400, 400);
+            setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+            setLocationRelativeTo(null);
+            setupPolygon(a);
+
+	}
+        
+        void setupPolygon(int [] a) {
+            System.out.println("I entered setupPolygon");
+            //Angles Array
+            //Divide 360 degree circle by sides
+
+            Double theta = 2*Math.PI/sides;
+            Double [] angles = new Double[sides];
+            Double arc = 0.00;
+            for ( int i = 0; i < sides; i++) {
+                angles[i] = arc;
+                arc = theta + arc;
+            }
+            for ( int i = 0; i < sides; i++) {
+                permvector[i] = a[i];
+            }
+            for ( int i = 0; i < sides; i++) {
+                Double x = (Math.cos(angles[i])*radius) + startingX;
+                Double y = (Math.sin(angles[i])*radius) + startingY;
+                xarray[i] = x;
+                yarray[i] = y;
+            }  
+        }
+
+	void drawLines(Graphics g) {
+            Graphics2D g2d = (Graphics2D) g;
+            System.out.println("I entered drawLines");
+            for (int point = 0; point < sides; point++) {
+		int mysub = permvector[point];
+		int nextsub;
+		if (point + 1 < sides) {
+			nextsub = permvector[point + 1];
+		}
+		else {
+			nextsub = permvector[0];
+			}
+		Double xco = xarray[mysub];
+		Double yco = yarray[mysub];
+		Double nxco = xarray[nextsub];
+		Double nyco = yarray[nextsub];
+		System.out.println("sub = " + mysub + " nextsub " + nextsub);
+		System.out.println("xco = " + xco + " yco = " + yco + " nxco = " + nxco + " nyco = " + nyco);
+		g2d.draw(new Line2D.Double(xco, yco, nxco, nyco));
+            }
+//            g2d.drawLine(120, 50, 360, 50);
+//            g2d.draw(new Line2D.Double(59.2d, 99.8d, 419.1d, 99.8d));
+//            g2d.draw(new Line2D.Float(21.50f, 132.50f, 459.50f, 132.50f));
+
+	}
+
+	public void paint(Graphics g) {
+		super.paint(g);
+		drawLines(g);
+	}
+        
+//        public static void main(String[] args) {
+//            SwingUtilities.invokeLater(new Runnable() {
+//                    @Override
+//                    public void run() {
+//                        int [] a = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11};
+//                        PolygonDrawing myp = new PolygonDrawing(a);
+//                        myp.setVisible(true);
+//                        try{Thread.sleep(250);}catch(InterruptedException e){System.out.println(e);}
+//                        myp.dispose();
+//                    }
+//            });
+//	}
+}
